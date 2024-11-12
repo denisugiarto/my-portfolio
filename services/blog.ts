b5a8184aac40297eee14f1da99e06ac29e43c239
deni sugiarto <sugiarto.deni@gmail.com>
2024-11-12 08:14:24 +0700

    feat(blogService):add fetch article by slug

--- services/blog.ts
+++ services/blog.ts
@@ -4,7 +4,19 @@ import axios from "axios";
 export const fetchArticles = async (): Promise<Article[]> => {
   try {
     const { data } = await axios.get<Article[]>(
-      "https://dev.to/api/articles?username=deni_sugiarto_1a01ad7c3fb",
+      `https://dev.to/api/articles?username=${process.env.NEXT_PUBLIC_DEV_USERNAME}`,
+    );
+    return data;
+  } catch (error: any) {
+    console.log(error);
+    throw new Error(error.response?.data?.message || "Error fetching article");
+  }
+};
+
+export const fetchArticleBySlug = async (slug: string): Promise<Article> => {
+  try {
+    const { data } = await axios.get<Article>(
+      `https://dev.to/api/articles/${process.env.NEXT_PUBLIC_DEV_USERNAME}/${slug}`,
     );
     return data;
   } catch (error: any) {
