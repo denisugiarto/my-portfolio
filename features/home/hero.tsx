cfca96aff79698d98a8fbf90731c683a6df6f0cf
deni sugiarto <sugiarto.deni@gmail.com>
2024-11-20 22:34:14 +0700

    feat(hero):adjust CTA button

--- features/home/hero.tsx
+++ features/home/hero.tsx
@@ -5,13 +5,12 @@ import Image from "next/image";
 
 // eslint-disable-next-line react/display-name
 export const linkHireMe = data.contact?.find(
-  (contact) => contact.type === "whatsapp",
+  (contact) => contact.type === "email",
 )?.link;
 type HeroProps = {
-  sectionBgColor: string;
   sectionColor: string;
 };
-const Hero = ({ sectionBgColor, sectionColor }: HeroProps) => {
+const Hero = ({ sectionColor }: HeroProps) => {
   return (
     <LazyMotion features={domAnimation}>
       <div className={`text-${sectionColor} container lg:py-32`}>
@@ -33,16 +32,31 @@ const Hero = ({ sectionBgColor, sectionColor }: HeroProps) => {
               </m.span>
               ,<span className="ml-6 text-2xl"> I&apos;m Deni Sugiarto</span>
             </h1>
-            <p className="text-4xl font-bold">
-              Frontend Web Developer
-            </p>
+            <p className="text-4xl font-bold">Frontend Web Developer</p>
             <p className="mt-6 leading-loose">{data.aboutMe}</p>
             <div className="mt-12">
               <a
                 href={linkHireMe}
-                className={`easy-in-out block transform rounded-xl border-2 px-20 py-3 text-center font-serif font-bold  capitalize transition duration-300 hover:shadow-card md:inline bg-${sectionBgColor} text-${sectionColor}`}
+                className="group relative block w-fit cursor-pointer border-none bg-transparent p-0 font-body text-base font-medium uppercase outline-none"
               >
-                Let&apos;s Talk!
+                <span className="duration-[600ms] ease-[cubic-bezier(0.3,0.7,0.4,1)] group-hover:duration-[250ms] absolute left-0 top-0 h-full w-full translate-y-0.5 transform rounded-lg bg-black bg-opacity-25 transition group-hover:translate-y-1 group-active:translate-y-px"></span>
+
+                <span className="absolute left-0 top-0 h-full w-full animate-pulse rounded-lg bg-gradient-to-l from-[hsl(217,33%,16%)] via-[hsl(217,33%,32%)]  to-[hsl(217,33%,16%)]"></span>
+
+                <div className="duration-[600ms] ease-[cubic-bezier(0.3,0.7,0.4,1)] group-hover:duration-[250ms] relative flex -translate-y-1 transform items-center justify-between gap-3 rounded-lg border border-gray-700 bg-gradient-to-r from-primary to-blue-800 px-6 py-3 text-lg text-white brightness-100 transition group-hover:-translate-y-1.5 group-hover:brightness-110 group-active:-translate-y-0.5">
+                  <span className="select-none">Let&apos;s Talk!</span>
+                  <svg
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                    className="duration-250 -mr-1 ml-2 h-5 w-5 transition group-hover:translate-x-1"
+                  >
+                    <path
+                      clip-rule="evenodd"
+                      d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                      fill-rule="evenodd"
+                    ></path>
+                  </svg>
+                </div>
               </a>
             </div>
             <div className="absolute right-10 top-0">
