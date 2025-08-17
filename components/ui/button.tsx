a7fb59c42afa7a07d8935dd1e37370176f13b2ff
deni sugiarto <sugiarto.deni@gmail.com>
2025-08-17 16:08:30 +0700

    feat: add size xl and remove height with padding for clear size rules

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -20,9 +20,10 @@ const buttonVariants = cva(
         link: "text-primary underline-offset-4 hover:underline",
       },
       size: {
-        default: "h-10 px-4 py-2",
-        sm: "h-9 rounded-md px-3",
-        lg: "h-11 rounded-md px-8",
+        default: "px-4 py-2",
+        sm: "rounded-md px-3 py-2 text-xs",
+        lg: "rounded-md px-8 py-4 text-lg",
+        xl: "rounded-md px-10 py-4 text-xl",
         icon: "h-10 w-10",
       },
     },
