59eae1778813bc6d3ddd4824cb6da0ae027d494d
deni sugiarto <sugiarto.deni@gmail.com>
2025-08-19 11:50:03 +0700

    fix:change dropdown theme to theme toggle

--- components/Layout/ThemeToggle.tsx
+++ components/Layout/ThemeToggle.tsx
@@ -1,57 +1,32 @@
 "use client";
 import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
 import { MoonIcon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
-import { useState } from "react";
-import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
-import SimpleTooltip from "../ui/simple-tooltip";
 
 const ThemeToggle = () => {
-  const [isOpen, setIsOpen] = useState(false);
-  const { theme, setTheme, resolvedTheme } = useTheme();
+  const { setTheme, resolvedTheme } = useTheme();
 
-  function setThemeMode(theme: string) {
-    setTheme(theme);
-    setIsOpen(false);
-  }
-
-  const themeVariant = [
-    {
-      name: "light",
-      caption: "Light",
-    },
-    {
-      name: "dark",
-      caption: "Dark",
-    },
-    {
-      name: "system",
-      caption: "System",
-    },
-  ];
+  const handleThemeToggle = () => {
+    const isDark = resolvedTheme === "dark";
+    setTheme(isDark ? "light" : "dark");
+  };
 
   return (
-    <Popover open={isOpen}>
-      <PopoverTrigger
-        onClick={() => setIsOpen(!isOpen)}
-        className={"rounded-md bg-primary p-1 text-slate-50"}
-        aria-label="Toggle Theme"
-        title="Toggle Theme"
-      >
-        {resolvedTheme === "dark" ? <MoonIcon /> : <Sun />}
-      </PopoverTrigger>
-      <PopoverContent className="flex w-auto flex-col !p-0">
-        {themeVariant.map((item) => (
-          <Button
-            key={item.name}
-            variant={theme === item.name ? "default" : "ghost"}
-            onClick={() => setThemeMode(item.name)}
-          >
-            {item.caption}
-          </Button>
-        ))}
-      </PopoverContent>
-    </Popover>
+    <Button
+      variant="ghost"
+      size="icon"
+      color="primary"
+      onClick={handleThemeToggle}
+      className={cn(
+        resolvedTheme === "dark" ? "text-slate-50" : "text-slate-950",
+        "rounded-md p-1",
+      )}
+      aria-label="Toggle Theme"
+      title="Toggle Theme"
+    >
+      {resolvedTheme === "dark" ? <MoonIcon /> : <Sun />}
+    </Button>
   );
 };
 
