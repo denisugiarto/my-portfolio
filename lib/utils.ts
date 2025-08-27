bb26f3b50855d02a335f9a8b782289f21ca97690
deni sugiarto <sugiarto.deni@gmail.com>
2025-08-27 14:13:28 +0700

    feat(utils):change cn with twMerge and clsx

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,3 +1,11 @@
-export function cn(...classes: string[]) {
-  return classes.filter(Boolean).join(" ");
-}
+import { ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+/**
+ * Combines clsx and tailwind-merge for optimal class merging.
+ * @param inputs - Class names or conditional class values.
+ * @returns A single string with merged class names.
+ */
+export const cn = (...inputs: ClassValue[]): string => {
+  return twMerge(clsx(inputs));
+};
