040bd8557e3cb3a028d6917cce64f913be0c85ce
deni sugiarto <sugiarto.deni@gmail.com>
2025-08-28 11:23:53 +0700

    fix(googleAnalytics):use @next/third-parties library for handle google analytics

--- components/analytics/GoogleAnalytics.tsx
+++ components/analytics/GoogleAnalytics.tsx
@@ -1,69 +1,13 @@
-'use client'
+import { GoogleAnalytics } from '@next/third-parties/google'
 
-import { useEffect } from 'react'
-import { usePathname } from 'next/navigation'
-import Script from 'next/script'
-
-interface GoogleAnalyticsProps {
+interface GoogleAnalyticsComponentProps {
   measurementId: string
 }
 
-// Declare gtag function for TypeScript
-declare global {
-  interface Window {
-    gtag: (...args: any[]) => void
-    dataLayer: any[]
+export default function GoogleAnalyticsComponent({ measurementId }: GoogleAnalyticsComponentProps) {
+  if (!measurementId) {
+    return null
   }
-}
 
-export default function GoogleAnalytics({ measurementId }: GoogleAnalyticsProps) {
-  const pathname = usePathname()
-
-  useEffect(() => {
-    if (!measurementId || typeof window === 'undefined') {
-      return
-    }
-
-    // Initialize dataLayer if not already present
-    window.dataLayer = window.dataLayer || []
-    
-    // Define gtag function
-    function gtag(...args: any[]) {
-      window.dataLayer.push(args)
-    }
-
-    // Initialize Google Analytics
-    gtag('js', new Date())
-    gtag('config', measurementId, {
-      page_title: document.title,
-      page_location: window.location.href,
-      send_page_view: true
-    })
-
-    // Make gtag available globally
-    window.gtag = gtag
-  }, [measurementId])
-
-  // Track route changes
-  useEffect(() => {
-    if (typeof window !== 'undefined' && window.gtag && pathname) {
-      window.gtag('config', measurementId, {
-        page_title: document.title,
-        page_location: window.location.href
-      })
-    }
-  }, [pathname, measurementId])
-
-  return (
-    <>
-      {/* Google Analytics Script */}
-      <Script
-        src={`https://www.googletagmanager.com/gtag/js?id=${measurementId}`}
-        strategy="afterInteractive"
-        onError={(error) => {
-          console.error('Failed to load Google Analytics:', error)
-        }}
-      />
-    </>
-  )
+  return <GoogleAnalytics gaId={measurementId} />
 }
